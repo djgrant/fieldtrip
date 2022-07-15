CodeYourFuture/fieldtrip-js2-coworkers

chore: refactor folder structure

apps/server/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import * as routers from "./routers";
-import * as bots from "./bots";
 import * as mw from "./middlewares";
 
 export const app = express();
@@ -8,13 +7,9 @@ export const app = express();
 app.use(mw.cors);
 app.use(mw.locals);
 app.use(mw.session);
+app.use([mw.cyf, mw.amber, mw.malachi, mw.uma]);
+
 app.use(["/api/user", "/api/courses/:id"], mw.userSession);
 app.use("/api/courses/:id", [mw.course, mw.botSessions, mw.store]);
-
-app.use(mw.probot(bots.cyf));
-app.use(mw.probot(bots.amber));
-app.use(mw.probot(bots.malachi));
-app.use(mw.probot(bots.uma));
-
 app.use("/api", routers.api);
 app.use("/auth", routers.auth);

apps/server/src/middlewares/bots.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createNodeMiddleware } from "probot";
+import * as bots from "../services/bots";
 import type { Probot } from "probot";
 import type { ApplicationFunction } from "probot/lib/types";
 import type { BotConfig } from "../config";
 
-export const probot = (params: {
+const probot = (params: {
   app: ApplicationFunction;
   instance: Probot;
   config: BotConfig;
@@ -16,3 +17,8 @@ export const probot = (params: {
     })
   );
 };
+
+export const cyf = probot(bots.cyf);
+export const malachi = probot(bots.malachi);
+export const uma = probot(bots.uma);
+export const amber = probot(bots.amber);

apps/server/src/services/bots.ts
@@ -1,0 +1,6 @@
+import { createBot } from "./bot";
+
+export const cyf = createBot("cyf");
+export const malachi = createBot("malachi");
+export const uma = createBot("uma");
+export const amber = createBot("amber");

djgrant/fieldtrip

apps/server/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import * as routers from "./routers";
-import * as bots from "./bots";
 import * as mw from "./middlewares";
 
 export const app = express();
@@ -8,13 +7,9 @@ export const app = express();
 app.use(mw.cors);
 app.use(mw.locals);
 app.use(mw.session);
+app.use([mw.fieldtrip, mw.amber, mw.malachi, mw.uma]);
+
 app.use(["/api/user", "/api/courses/:id"], mw.userSession);
 app.use("/api/courses/:id", [mw.course, mw.botSessions, mw.store]);
-
-app.use(mw.probot(bots.cyf));
-app.use(mw.probot(bots.amber));
-app.use(mw.probot(bots.malachi));
-app.use(mw.probot(bots.uma));
-
 app.use("/api", routers.api);
 app.use("/auth", routers.auth);

apps/server/src/middlewares/bots.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createNodeMiddleware } from "probot";
+import * as bots from "../services/bots";
 import type { Probot } from "probot";
 import type { ApplicationFunction } from "probot/lib/types";
 import type { BotConfig } from "../config";
 
-export const probot = (params: {
+const probot = (params: {
   app: ApplicationFunction;
   instance: Probot;
   config: BotConfig;
@@ -16,3 +17,8 @@ export const probot = (params: {
     })
   );
 };
+
+export const fieldtrip = probot(bots.fieldtrip);
+export const malachi = probot(bots.malachi);
+export const uma = probot(bots.uma);
+export const amber = probot(bots.amber);

apps/server/src/services/bots.ts
@@ -1,0 +1,6 @@
+import { createBot } from "./bot";
+
+export const fieldtrip = createBot("fieldtrip");
+export const malachi = createBot("malachi");
+export const uma = createBot("uma");
+export const amber = createBot("amber");