CodeYourFuture/fieldtrip-js2-coworkers

feat: pass data from hooks to milestones/actions

apps/server/src/bots/index.ts
@@ -1,4 +1,59 @@
-export * as cyf from "./cyf";
-export * as amber from "./amber";
-export * as malachi from "./malachi";
-export * as uma from "./uma";
+import { Probot } from "probot";
+import PQueue from "p-queue";
+import courses from "@packages/courses";
+import type { Bots } from "@packages/courses/types";
+import { Course, Store, createProbot, Bot } from "../utils";
+import { bots } from "../config";
+
+const course = courses.js2;
+const repo = "js2";
+
+const hooks = Course.getHooks(course);
+const hooksByBotName = hooks.reduce((acc, hook) => {
+  if (!acc[hook.hook.botName]) acc[hook.hook.botName] = [];
+  acc[hook.hook.botName].push(hook);
+  return acc;
+}, {} as Record<string, ReturnType<typeof Course.getHooks>>);
+
+const createBot = (botName: Bots) => {
+  const botHooks = hooksByBotName[botName];
+  const q = new PQueue({ concurrency: 1 });
+
+  const app = (app: Probot) => {
+    if (!botHooks) return;
+
+    for (const hook of botHooks) {
+      const { event, predicate, action } = hook.hook;
+
+      app.on(event as any, async (context) => {
+        const bot = new Bot(context, repo);
+        if (bot.eventShouldBeIgnored) return;
+        const store = new Store(context.repo());
+        const state = await store.getAll();
+
+        const passed = predicate(context.payload, state, bot);
+        if (!passed) return;
+
+        if (!action) {
+          await store.add("passed", hook.id);
+        } else {
+          await q.add(async () => {
+            const result = await action(bot, await store.getAll());
+            await store.set(["hooks", hook.id], result);
+          });
+        }
+      });
+    }
+  };
+
+  return {
+    app,
+    instance: createProbot(bots[botName]),
+    config: bots[botName],
+  };
+};
+
+export const cyf = createBot("cyf");
+export const malachi = createBot("malachi");
+export const uma = createBot("uma");
+export const amber = createBot("amber");

apps/server/src/types/index.ts
@@ -1,9 +1,6 @@
-import * as bots from "../bots";
-
 export * from "./course";
 export * from "./locals";
 export * from "./store";
 export * from "./session";
 
-export type Bots = typeof bots;
-// export type Bots = "cyf" | "malachi" | "amber" | "uma";
+export type Bots = "cyf" | "malachi" | "amber" | "uma";

apps/server/src/utils/course-utils.ts
@@ -3,15 +3,37 @@ import type {
   EmitterWebhookEvent,
 } from "@octokit/webhooks";
 import { StoreData } from "@packages/courses/types";
+import { Bot } from "../utils";
+import type { Hook, EventAssertion } from "../types/course";
 
-export function on<EventName extends EmitterWebhookEventName>(
-  event: EventName,
-  handler: HandlerFunction<EventName>
-) {
-  return { event, handler };
-}
-
-export type HandlerFunction<TName extends EmitterWebhookEventName> = (
-  event: EmitterWebhookEvent<TName>["payload"],
+export type Predicate<E extends EmitterWebhookEventName> = (
+  event: EmitterWebhookEvent<E>["payload"],
   state: StoreData
 ) => boolean;
+
+export type Action = (bot: Bot, state: StoreData) => Promise<unknown>;
+
+export function on<E extends EmitterWebhookEventName>(
+  event: E | E[],
+  predicate: Predicate<E>
+): EventAssertion;
+
+export function on<E extends EmitterWebhookEventName>(
+  event: E | E[],
+  predicate: Predicate<E>,
+  action: Action
+): Hook;
+
+export function on<E extends EmitterWebhookEventName>(
+  this: string,
+  event: string | string[],
+  predicate: (...args: any) => boolean,
+  action?: (...args: any) => Promise<unknown>
+): unknown {
+  const botName = this || "cyf";
+  return { event, predicate, action, botName };
+}
+
+on.amber = on.bind("amber");
+on.malachi = on.bind("malachi");
+on.uma = on.bind("uma");

djgrant/fieldtrip

apps/server/src/bots/index.ts
@@ -1,4 +1,59 @@
-export * as cyf from "./cyf";
-export * as amber from "./amber";
-export * as malachi from "./malachi";
-export * as uma from "./uma";
+import { Probot } from "probot";
+import PQueue from "p-queue";
+import courses from "@packages/courses";
+import type { Bots } from "@packages/courses/types";
+import { Course, Store, createProbot, Bot } from "../utils";
+import { bots } from "../config";
+
+const course = courses.js2;
+const repo = "js2";
+
+const hooks = Course.getHooks(course);
+const hooksByBotName = hooks.reduce((acc, hook) => {
+  if (!acc[hook.hook.botName]) acc[hook.hook.botName] = [];
+  acc[hook.hook.botName].push(hook);
+  return acc;
+}, {} as Record<string, ReturnType<typeof Course.getHooks>>);
+
+const createBot = (botName: Bots) => {
+  const botHooks = hooksByBotName[botName];
+  const q = new PQueue({ concurrency: 1 });
+
+  const app = (app: Probot) => {
+    if (!botHooks) return;
+
+    for (const hook of botHooks) {
+      const { event, predicate, action } = hook.hook;
+
+      app.on(event as any, async (context) => {
+        const bot = new Bot(context, repo);
+        if (bot.eventShouldBeIgnored) return;
+        const store = new Store(context.repo());
+        const state = await store.getAll();
+
+        const passed = predicate(context.payload, state, bot);
+        if (!passed) return;
+
+        if (!action) {
+          await store.add("passed", hook.id);
+        } else {
+          await q.add(async () => {
+            const result = await action(bot, await store.getAll());
+            await store.set(["hooks", hook.id], result);
+          });
+        }
+      });
+    }
+  };
+
+  return {
+    app,
+    instance: createProbot(bots[botName]),
+    config: bots[botName],
+  };
+};
+
+export const fieldtrip = createBot("fieldtrip");
+export const malachi = createBot("malachi");
+export const uma = createBot("uma");
+export const amber = createBot("amber");

apps/server/src/types/index.ts
@@ -1,9 +1,6 @@
-import * as bots from "../bots";
-
 export * from "./course";
 export * from "./locals";
 export * from "./store";
 export * from "./session";
 
-export type Bots = typeof bots;
-// export type Bots = "fieldtrip" | "malachi" | "amber" | "uma";
+export type Bots = "fieldtrip" | "malachi" | "amber" | "uma";

apps/server/src/utils/course-utils.ts
@@ -3,15 +3,37 @@ import type {
   EmitterWebhookEvent,
 } from "@octokit/webhooks";
 import { StoreData } from "@packages/courses/types";
+import { Bot } from "../utils";
+import type { Hook, EventAssertion } from "../types/course";
 
-export function on<EventName extends EmitterWebhookEventName>(
-  event: EventName,
-  handler: HandlerFunction<EventName>
-) {
-  return { event, handler };
-}
-
-export type HandlerFunction<TName extends EmitterWebhookEventName> = (
-  event: EmitterWebhookEvent<TName>["payload"],
+export type Predicate<E extends EmitterWebhookEventName> = (
+  event: EmitterWebhookEvent<E>["payload"],
   state: StoreData
 ) => boolean;
+
+export type Action = (bot: Bot, state: StoreData) => Promise<unknown>;
+
+export function on<E extends EmitterWebhookEventName>(
+  event: E | E[],
+  predicate: Predicate<E>
+): EventAssertion;
+
+export function on<E extends EmitterWebhookEventName>(
+  event: E | E[],
+  predicate: Predicate<E>,
+  action: Action
+): Hook;
+
+export function on<E extends EmitterWebhookEventName>(
+  this: string,
+  event: string | string[],
+  predicate: (...args: any) => boolean,
+  action?: (...args: any) => Promise<unknown>
+): unknown {
+  const botName = this || "fieldwork";
+  return { event, predicate, action, botName };
+}
+
+on.amber = on.bind("amber");
+on.malachi = on.bind("malachi");
+on.uma = on.bind("uma");