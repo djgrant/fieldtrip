e8dedaad1194e1248aab889072f7a6d42d42d5b0
Alaa Yahia <alaay873@live.com>
2022-08-05 10:47:06 +0200

    Adding fieldtrip package

--- apps/server/src/io.ts
+++ apps/server/src/io.ts
@@ -6,6 +6,7 @@ import * as mw from "./middlewares";
 import * as config from "./config";
 import { Course } from "./services";
 import { Enrollments } from "./types";
+import * as fieldtrip from "@notation/fieldtrip";
 
 export const io = (server: HTTPServer) => {
   const io = new Server(server, {
