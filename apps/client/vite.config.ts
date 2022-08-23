b44992a3c44794d86bce384afd048a88dc80a31e
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-08-23 18:08:24 +0100

    update dockerfiles

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     target: "es2022",
+    outDir: "../../dist/apps/client",
   },
   define: {
     DEV: process.env.DEV,
