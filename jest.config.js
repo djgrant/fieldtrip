4745131ac172854c15b08fc2e6cb447b14ff52b6
Alaa Yahia <alaay873@live.com>
2022-10-11 13:35:20 +0200

    Run postegres docker image

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
+  roots: ["<rootDir>apps/server/tests/"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "node",
-  projects: '<rootDir>/$"14.4.0-beta.5"root}',
 };
