826d3e62aff7f105569447de5904ffc39f40ac3c
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-11-04 09:35:28 +0000

    Add package to nock github contents API

---
+++ packages/@local/nock-github-contents/index.ts
@@ -0,0 +1,64 @@
+import nock from "nock";
+import { join as pathJoin } from "path";
+import { readFile, readdir } from "fs/promises";
+import { Dirent } from "fs";
+
+/**
+ * @description Generates nocks to Github Contents API for every file and directory within @param absPathToDir
+ */
+export async function generateGhContentsApiNocks(absPathToDir: string) {
+  const getDirents = (relativeRootPath: string) => {
+    const absPath = pathJoin(absPathToDir, relativeRootPath);
+    return readdir(absPath, { withFileTypes: true });
+  };
+  async function generateNocks(relativeRootPath: string = ".") {
+    const dirents = await getDirents(relativeRootPath);
+    dirents.forEach(async (dirent) => {
+      const direntPath = pathJoin(relativeRootPath, dirent.name);
+      if (dirent.isDirectory()) {
+        const childDirents = await getDirents(direntPath);
+        generateDirNock(direntPath, childDirents);
+        generateNocks(direntPath);
+      } else {
+        const contents = await readFile(pathJoin(absPathToDir, direntPath));
+        generateFileNock(direntPath, dirent.name, contents);
+      }
+    });
+  }
+  generateNocks();
+}
+
+function generateFileNock(path: string, filename: string, contents: Buffer) {
+  nock("https://api.github.com")
+    .get(`/repos/org/repo/contents${path}`)
+    .reply(200, getFileResponse(path, filename, contents));
+}
+
+function generateDirNock(path: string, dirents: Dirent[]) {
+  nock("https://api.github.com")
+    .get(`/repos/org/repo/contents${path}`)
+    .reply(200, getDirResponse(path, dirents));
+}
+
+function getFileResponse(filePath: string, filename: string, content: Buffer) {
+  return {
+    type: "file",
+    name: filename,
+    path: filePath,
+    url: `https://api.github.com/repos/org/course/contents/${filePath}`,
+    content: Buffer.from(content).toString("base64"),
+    encoding: "base64",
+  };
+}
+
+function getDirResponse(dirPath: string, dirents: Dirent[]) {
+  return dirents.map((dirent) => ({
+    type: dirent.isDirectory() ? "dir" : "file",
+    name: dirent.name,
+    path: pathJoin(dirPath, dirent.name),
+    url: `https://api.github.com/repos/org/course/contents/${pathJoin(
+      dirPath,
+      dirent.name
+    )}`,
+  }));
+}
